a33427d984d29c8a8c56d5b27874d00d21903bce
Yitpin Chin <yitpin.dev@gmail.com>
2020-06-11 00:02:19 +0100

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,27 +3,37 @@ import Person from './components/Person';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '123456789'
+    }
   ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} has already been added to the phonebook!`);
       setNewName('');
+      setNewNumber('');
     } else {
       const personObject = {
         name: newName,
+        number: newNumber,
       }
 
       setNewName('');
+      setNewNumber('');
       setPersons(persons.concat(personObject));
     }
   }
 
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -31,7 +41,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} type="number" />
         </div>
         <div>
           <button type="submit">Add</button>
