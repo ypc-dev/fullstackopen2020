65e3c1f70be285114ca71d1e6da2b4ce4c28f449
Yitpin Chin <yitpin.dev@gmail.com>
2020-06-16 23:08:55 +0100

    Exercise 3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -68,6 +68,35 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  const min = 1;
+  const max = 100000;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    });
+  }
+  console.log(body.content)
+  console.log(body.name)
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
 
 
 const PORT = 3001;
